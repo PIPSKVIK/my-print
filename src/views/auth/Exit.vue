cbfee2ea8b691fe0c883e58fcbd98d02a451993f
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-09-14 11:46:49 +0300

    Очистка компонента Exit после тестов

--- src/views/auth/Exit.vue
+++ src/views/auth/Exit.vue
@@ -1,36 +1,13 @@
 <template>
   <div class="exit">
     <h2>Exit</h2>
-    <div>
-      <BaseInput
-        type="password"
-        name="Password"
-        placeholder="Password"
-        class="exit-password"
-        v-model="password"
-        :passwordData="password"
-      >
-      </BaseInput>
-      <BaseInput
-        type="text"
-      />
-    </div>
   </div>
 </template>
 
 <script>
-import BaseInput from '@/components/baseUi/BaseInput'
 
 export default {
-  name: 'Exit',
-  components: {
-    BaseInput
-  },
-  data () {
-    return {
-      password: ''
-    }
-  }
+  name: 'Exit'
 }
 </script>
 
